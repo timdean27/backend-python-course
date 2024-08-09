03b83c7cbb91ea059d463ae64892b2cb7bafc4a3
timdean27 <timdean27@yahoo.com>
2024-08-09 19:08:33 -0400

    finaly have static files uploading running on RDS, and able to download images

--- mainProject/mainProject/settings.py
+++ mainProject/mainProject/settings.py
@@ -147,10 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = AWS_S3_CUSTOM_DOMAIN + '/static/'
-# STATIC_ROOT = AWS_S3_CUSTOM_DOMAIN
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_ROOT = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
